13ae9ff20cc74ba39f42a1ba0f4784c94a5b9fb7
chenzx <lyxx2413@outlook.com>
2025-02-13 15:09:31 +0800

    profile: add trap ^C on test script

--- src/raft/test.sh
+++ src/raft/test.sh
@@ -1,13 +1,25 @@
 #!/usr/bin/env sh
 
-errori=0
+trap 'echo "Cancel"; exit 1' INT TERM
 
-if [ "$#" -ne 1 ]; then
-  flags="-run $*"
+if [ "$#" -eq 1 ]; then
+  flags="-run $1"
+elif [ "$#" -gt 1 ]; then
+  echo "Usage: $0 [test-regex]"
+  exit 1
+else
+  flags=""
 fi
+
+test -d log || mkdir log
+rm -rf log/*
+
+errori=0
 
 for t in $(seq 1 50); do
   echo "start test $t"
-  go test -race "$flags" > "log/error-${errori}.log" 2>&1 || errori=$((errori + 1))
+  go test -race $flags > "log/error-${errori}.log" 2>&1 || errori=$((errori + 1))
 done
+
+exit $errori
 
