2bec68fadb9d78ebfdda50ea20c4ef4549c0564f
chenzx <lyxx2413@outlook.com>
2025-02-14 17:23:20 +0800

    profile: improve printf in test script

--- src/raft/test.sh
+++ src/raft/test.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env sh
 
-trap 'echo "Cancel"; exit 1' INT TERM
+trap 'exit 1' INT TERM
 
-if [ "$#" -eq 1 ]; then
-  flags="-run $1"
-elif [ "$#" -gt 1 ]; then
-  echo "Usage: $0 [test-regex]"
+echo_red() {
+  printf "\033[31m%s\033[0m" "$*"
+}
+
+echo_green() {
+  printf "\033[32m%s\033[0m\n" "$*"
+}
+
+if [ "$#" -eq 2 ]; then
+  flags="-run $2"
+elif [ "$#" -gt 2 ]; then
+  echo "Usage: $0 [test-times] [test-regex]"
   exit 1
 else
   flags=""
@@ -16,10 +24,14 @@ rm -rf log/*
 
 errori=0
 
-for t in $(seq 1 50); do
-  echo "start test $t"
-  go test -race $flags > "log/error-${errori}.log" 2>&1 || errori=$((errori + 1))
+for t in $(seq 0 "$1"); do
+  printf "test%s..." "$t"
+  if go test -race $flags >"log/error-${errori}.log" 2>&1; then
+    echo_green "OK"
+  else
+    errori=$((errori + 1))
+    echo_red "FAIL"
+  fi
 done
 
 exit $errori
-
